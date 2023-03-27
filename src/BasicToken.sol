98453e3e03dbb7bdd50523aa850e95b6dc5b53c9
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-03-27 16:08:42 +0300

    Add useraccount and test deposit and withdraw

--- src/BasicToken.sol
+++ src/BasicToken.sol
@@ -2,11 +2,27 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 // Example class - a mock class using delivering from ERC20
-contract BasicToken is ERC20 {
-  constructor(uint256 initialBalance) ERC20("Basic", "BSC") {
-      _mint(msg.sender, initialBalance);
-  }
+contract UserAccount { 
+    mapping(address => uint256) 
+    public balances; mapping(address => Transaction[]) 
+    public transactions; 
+    struct Transaction { address sender; address receiver; uint256 amount; uint256 timestamp; } 
+    function deposit() public payable { 
+        balances[msg.sender] += msg.value; 
+        transactions[msg.sender].push(Transaction(msg.sender, address(this), msg.value, block.timestamp));
+         } 
+    function withdraw(uint256 amount) public { 
+        require(balances[msg.sender] >= amount, "Insufficient balance"); balances[msg.sender] -= amount; 
+        payable(msg.sender).transfer(amount); 
+        transactions[msg.sender].push(Transaction(msg.sender, address(this), amount, block.timestamp)); 
+        } 
+    function transfer(address receiver, uint256 amount) public { 
+        require(balances[msg.sender] >= amount, "Insufficient balance"); 
+        balances[msg.sender] -= amount; balances[receiver] += amount; 
+        transactions[msg.sender].push(Transaction(msg.sender, receiver, amount, block.timestamp)); 
+        } 
+    function getTransactions() public view returns (Transaction[] memory) { 
+        return transactions[msg.sender]; 
+        } 
 }
--- test/BasicToken.test.ts
+++ test/BasicToken.test.ts
@@ -1,50 +1,56 @@
 import {expect, use} from 'chai';
 import {Contract} from 'ethers';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
-import BasicToken from '../build/BasicToken.json';
+import UserAccount from '../build/UserAccount.json';
 
 use(solidity);
 
-describe('BasicToken', () => {
+describe('UserAccount', () => {
   const [wallet, walletTo] = new MockProvider().getWallets();
   let token: Contract;
 
   beforeEach(async () => {
-    token = await deployContract(wallet, BasicToken, [1000]);
+    token = await deployContract(wallet, UserAccount);
+  });
+  
+  it('Get initial balance', async () => {
+    expect(await token.balances(wallet.address)).to.equal(0);
   });
 
-  it('Assigns initial balance', async () => {
-    expect(await token.balanceOf(wallet.address)).to.equal(1000);
+  it('Deposit amount into the wallet', async () => {
+    await token.deposit({value: 100});
+    expect(await token.balances(wallet.address)).to.equal(100);
   });
 
-  it('Transfer adds amount to destination account', async () => {
-    await token.transfer(walletTo.address, 7);
-    expect(await token.balanceOf(walletTo.address)).to.equal(7);
+  it('Deposit then withdraw some from the wallet', async () => {
+    await token.deposit({value: 150});
+    await token.withdraw(50);
+    expect(await token.balances(wallet.address)).to.equal(100);
   });
 
-  it('Transfer emits event', async () => {
-    await expect(token.transfer(walletTo.address, 7))
-      .to.emit(token, 'Transfer')
-      .withArgs(wallet.address, walletTo.address, 7);
-  });
+  // it('Transfer emits event', async () => {
+  //   await expect(token.transfer(walletTo.address, 7))
+  //     .to.emit(token, 'Transfer')
+  //     .withArgs(wallet.address, walletTo.address, 7);
+  // });
 
-  it('Can not transfer above the amount', async () => {
-    await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
-  });
+  // it('Can not transfer above the amount', async () => {
+  //   await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
+  // });
 
-  it('Can not transfer from empty account', async () => {
-    const tokenFromOtherWallet = token.connect(walletTo);
-    await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
-      .to.be.reverted;
-  });
+  // it('Can not transfer from empty account', async () => {
+  //   const tokenFromOtherWallet = token.connect(walletTo);
+  //   await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
+  //     .to.be.reverted;
+  // });
 
-  it('Calls totalSupply on BasicToken contract', async () => {
-    await token.totalSupply();
-    expect('totalSupply').to.be.calledOnContract(token);
-  });
+  // it('Calls totalSupply on BasicToken contract', async () => {
+  //   await token.totalSupply();
+  //   expect('totalSupply').to.be.calledOnContract(token);
+  // });
 
-  it('Calls balanceOf with sender address on BasicToken contract', async () => {
-    await token.balanceOf(wallet.address);
-    expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
-  });
+  // it('Calls balanceOf with sender address on BasicToken contract', async () => {
+  //   await token.balanceOf(wallet.address);
+  //   expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
+  // });
 });
