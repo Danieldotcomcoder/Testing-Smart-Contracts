7dc87cd8283db0498d8515fcd4e033816c68f10c
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-03-30 16:15:15 +0300

    fix test

--- test/Bankaccount.test.ts
+++ test/Bankaccount.test.ts
@@ -13,92 +13,60 @@ describe('Bank Account', () => {
     token = await deployContract(wallet, BankAccount);
   });
   
-  // it('create account', async () => {
-  //   expect(await token.createAccount());
-  // });
+  it('create account', async () => {
+    expect(await token.createAccount());
+  });
 
-  // it('get balance', async () => {
-  //   expect(await token.createAccount());
-  //   expect(await token.getBalance()).to.equal(0);
-  // });
-
-
-  // it('deposit and then getbalance', async () => {
-  //   expect(await token.createAccount());
-  //   expect(await token.deposit({value: 50}))
-  //   expect(await token.getBalance()).to.equal(50);
-  // });
+  it('get balance', async () => {
+    expect(await token.createAccount());
+    expect(await token.getBalance()).to.equal(0);
+  });
 
 
-  // it('deposit and withdraw then getbalance', async () => {
-  //   expect(await token.createAccount());
-  //   expect(await token.deposit({value: 50}))
-  //   expect(await token.withdraw(20))
-  //   expect(await token.getBalance()).to.equal(30);
-  // });
+  it('deposit and then getbalance', async () => {
+    expect(await token.createAccount());
+    expect(await token.deposit({value: 50}))
+    expect(await token.getBalance()).to.equal(50);
+  });
 
-  // it('Deposit then make a transaction then check balance', async () => {
-  //   expect(await token.createAccount());
-  //   await token.deposit({value: 150});
-  //   expect(await token.makeTransaction(50, {serviceName: 'Translating pdf'}, {serviceType: 'Translating'}));
-  //   expect(await token.getBalance()).to.equal(100);
-  // });
 
-  // it('make multiple transaction then get transactions', async () => {
-  //   expect(await token.createAccount());
-  //   await token.deposit({value: 150});
-  //   expect(await token.getBalance()).to.equal(150);
-  //   expect(await token.makeTransaction(50, 'Translating pdf', 'Translating'));
-  //   expect(await token.makeTransaction(10, 'Translating pdf', 'Translating'));
-  //   expect(await token.makeTransaction(10, 'Translating pdf', 'Translating'));
-  //   expect(await token.makeTransaction(10,  'Translating pdf','Translating'));
-  //   expect(await token.getBalance()).to.equal(70);
-  //   expect(await token.getTransactions()).to.be.an('array')
-  //   expect(await token.getTransactions()).to.have.lengthOf(4)
-  // });
+  it('deposit and withdraw then getbalance', async () => {
+    expect(await token.createAccount());
+    expect(await token.deposit({value: 50}))
+    expect(await token.withdraw(20))
+    expect(await token.getBalance()).to.equal(30);
+  });
 
-  // it('new account have an new address', async () => {
-  //   expect(await token.createAccount());
-  //   expect(token.address).to.equal('0x35dc06263b798f21C268d5aa4BEB73120413E829')
-   
-  // });
+  it('Deposit then make a transaction then check balance', async () => {
+    expect(await token.createAccount());
+    await token.deposit({value: 150});
+    expect(await token.makeTransaction(50, {serviceName: 'Translating pdf'}, {serviceType: 'Translating'}));
+    expect(await token.getBalance()).to.equal(100);
+  });
 
-  // it('new account have an new address', async () => {
-  //   expect(await token.createAccount());
-  //   expect(token.address).to.equal('0x3fD56895aF1191112A4aAe106050237452F3D028')
-   
-  // });
+  it('make multiple transaction then get transactions', async () => {
+    expect(await token.createAccount());
+    await token.deposit({value: 150});
+    expect(await token.getBalance()).to.equal(150);
+    expect(await token.makeTransaction(50, 'Translating pdf', 'Translating'));
+    expect(await token.makeTransaction(10, 'Translating pdf', 'Translating'));
+    expect(await token.makeTransaction(10, 'Translating pdf', 'Translating'));
+    expect(await token.makeTransaction(10,  'Translating pdf','Translating'));
+    expect(await token.getBalance()).to.equal(70);
+    expect(await token.getTransactions()).to.be.an('array')
+    expect(await token.getTransactions()).to.have.lengthOf(4)
+  });
+
   it('new account have an new address', async () => {
     expect(await token.createAccount());
-    expect(console.log(token.functions));
-    
+    expect(token.address).to.equal('0x35dc06263b798f21C268d5aa4BEB73120413E829')
    
   });
- 
 
-  // it('Transfer emits event', async () => {
-  //   await expect(token.transfer(walletTo.address, 7))
-  //     .to.emit(token, 'Transfer')
-  //     .withArgs(wallet.address, walletTo.address, 7);
-  // });
-
-  // it('Can not transfer above the amount', async () => {
-  //   await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
-  // });
-
-  // it('Can not transfer from empty account', async () => {
-  //   const tokenFromOtherWallet = token.connect(walletTo);
-  //   await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
-  //     .to.be.reverted;
-  // });
-
-  // it('Calls totalSupply on BasicToken contract', async () => {
-  //   await token.totalSupply();
-  //   expect('totalSupply').to.be.calledOnContract(token);
-  // });
-
-  // it('Calls balanceOf with sender address on BasicToken contract', async () => {
-  //   await token.balanceOf(wallet.address);
-  //   expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
-  // });
+  it('new account have an new address', async () => {
+    expect(await token.createAccount());
+    expect(token.address).to.equal('0x3fD56895aF1191112A4aAe106050237452F3D028')
+   
+  });
+  
 });
